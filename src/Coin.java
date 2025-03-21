ddb3e11875d8251ce78d823b84d47c0e893d5429
amandac4rr <156827477+amandac4rr@users.noreply.github.com>
2025-03-21 10:44:41 -0400

    coded Coin UML

--- src/Coin.java
+++ src/Coin.java
@@ -1,3 +1,27 @@
 public class Coin {
+    public double value; 
+    public String commonName; 
+    public String frontMotto;
+    public int manufactureYear;
+    public String frontImage;
+    public String backImage;
+    public String backMotto;
+    public String frontLabel;
+    public String backLabel;
+    public String valueDescription;
+    public boolean ridgedEdge;
+    public String matallurgy;
+
+    public  Coin()
+    {}    
+    
+    public Coin(double value)
+    {}
+
+    public Coin(double value, int year)
+    {}
+
+    public String toString()
+    {}
     
 }
