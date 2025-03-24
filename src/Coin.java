f22cf739f4543a83682f219ff99af05b420b674d
hlacsgit <hewittla@appstate.edu>
2025-03-24 18:16:51 -0400

    Begin implementing Coin constructor

--- src/Coin.java
+++ src/Coin.java
@@ -1,3 +1,5 @@
+import java.time.Year;
+
 public class Coin {
     private double value;
     private String commonName;
@@ -12,15 +14,101 @@ public class Coin {
     private boolean ridgedEdge;
     private String matallurgy;
 
+    public enum CoinType {
+        PENNY,
+        NICKEL,
+        DIME,
+        QUARTER,
+        HALF_DOLLAR,
+        DOLLAR,
+        UNKNOWN_COIN;
+
+        private static final CoinType[] COIN_TYPES = {
+            PENNY,
+            NICKEL,
+            DIME,
+            QUARTER,
+            HALF_DOLLAR,
+            DOLLAR
+        };
+
+        private static final double[] COIN_VALUES = {
+            0.01,
+            0.05,
+            0.10,
+            0.25,
+            0.50,
+            1.00
+        };
+
+        public static CoinType fromValue(double value) {
+            for (int i = 0; i < COIN_TYPES.length; i++) {
+                if (Math.abs(value - COIN_VALUES[i]) < 0.0001) {
+                    return COIN_TYPES[i];
+                }
+            }
+            return UNKNOWN_COIN;
+        }
+
+        public double getValue() {
+            for (int i = 0; i < COIN_TYPES.length; i++) {
+                if (this == COIN_TYPES[i]) {
+                    return COIN_VALUES[i];
+                }
+            }
+            return Double.NaN;
+        }
+    }
+
     public Coin() {
+        this(0.0);
     }
 
     public Coin(double value) {
-        this.value = value;
+        this(value, Year.now().getValue());
     }
 
     public Coin(double value, int year) {
-        this.value = value;
+        CoinType coinType = CoinType.fromValue(value);
+
+        switch (coinType) {
+            case PENNY:
+                // Penny
+                this.commonName = "Penny";
+                this.frontMotto = "IN GOD WE TRUST";
+                this.frontImage = "Abraham Lincoln";
+                this.frontLabel = "LIBERTY";
+                this.backMotto = "E PLURIBUS UNUM";
+                this.backImage = "Union Shield";
+                this.backLabel = "UNITED STATES OF AMERICA";
+                this.valueDescription = "ONE CENT";
+                this.ridgedEdge = false;
+                break;
+            case NICKEL:
+                // Nickel
+                this.ridgedEdge = false;
+                break;
+            case DIME:
+                // Dime
+                this.ridgedEdge = true;
+                break;
+            case QUARTER:
+                // Quarter
+                this.ridgedEdge = true;
+                break;
+            case HALF_DOLLAR:
+                // Half-dollar
+                this.ridgedEdge = true;
+                break;
+            case DOLLAR:
+                // Dollar
+                this.ridgedEdge = true;
+                break;
+            case UNKNOWN_COIN:
+                break;
+        }
+
+        this.value = coinType.getValue();
         this.manufactureYear = year;
     }
 
