6b93deb36893e644038649cf8efad2800b355cba
hlacsgit <hewittla@appstate.edu>
2025-03-24 15:38:02 -0400

    Add empty methods in CoinTest and Demo classes

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -1,3 +1,31 @@
 public class CoinTest {
-    
+    public Coin testPenny() {
+        return null;
+    }
+
+    public Coin testNickel() {
+        return null;
+    }
+
+    public Coin testDime() {
+        return null;
+    }
+
+    public Coin testQuarter() {
+        return null;
+    }
+
+    public Coin testHalfDollar() {
+        return null;
+    }
+
+    public Coin testDollar() {
+        return null;
+    }
+
+    public void testConstructors() {
+    }
+
+    public void testGetters() {
+    }
 }
